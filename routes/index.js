21411229893cdfa96fb6749c2e4de13a32cb3fbb
Anshu Rai <raianshu8991@gmail.com>
2021-12-25 21:03:56 +0530

    Adding home route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+router.get("/", (req, res) => res.json("Hello world"));
 router.use("/product", require("./product"));
 router.use("/user", require("./user"));
 
